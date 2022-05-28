9167afebb535254e6beb6c8d1a1b0ea439e27d97
josephtannoury <josephtannoury@outlook.com>
2022-05-28 21:29:46 +0300

    adding sign_up api

---
+++ js/sign_up.js
@@ -0,0 +1,22 @@
+let Full_name=document.getElementById("full_name");
+let E_mail=document.getElementById("email");
+let Password=document.getElementById("password");
+var id=0
+document.getElementById("btn").addEventListener("click",echo);
+function echo(){
+    let data = new FormData();
+            data.append('id', id);
+            data.append('full_name', Full_name.value);
+            data.append('email', email.value);
+            data.append('password', Password.value);
+            axios({
+                method: 'POST',
+                url: 'http://localhost/dbex/add_user.php',
+                data: data,
+            })
+            .then(function (response) {
+                console.log(response);
+                console.log(data)
+                }
+            )}
+
