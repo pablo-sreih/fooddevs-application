9fb327c0947c8e2b2af083815a537dfffd7831c0
josephtannoury <josephtannoury@outlook.com>
2022-05-28 23:51:30 +0300

    adding connections api

---
+++ API/connections.php
@@ -0,0 +1,9 @@
+<?php
+
+    $host="localhost";
+    $db_user="root";
+    $db_pass=null;
+    $db_name="fooddevs";
+    $mysqli=new mysqli($host,$db_user,$db_pass,$db_name);
+    echo "GGWP";
+    ?>
